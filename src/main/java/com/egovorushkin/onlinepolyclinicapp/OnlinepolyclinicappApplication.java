aecd8c1181453eebed9624b616e928f0e3177363
Evgenii <johnnywas87@gmail.com>
2019-12-13 22:14:35 +0300

    online-polyclinic-app

---
+++ src/main/java/com/egovorushkin/onlinepolyclinicapp/OnlinepolyclinicappApplication.java
@@ -0,0 +1,13 @@
+package com.egovorushkin.onlinepolyclinicapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinepolyclinicappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinepolyclinicappApplication.class, args);
+	}
+
+}
