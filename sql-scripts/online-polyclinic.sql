73b0f99c9467b43433b22f9352c0a1081a0f8307
Evgenii <johnnywas87@gmail.com>
2019-12-16 23:58:03 +0300

    some changes

--- sql-scripts/online-polyclinic.sql
+++ sql-scripts/online-polyclinic.sql
@@ -1,6 +1,8 @@
 CREATE DATABASE IF NOT EXISTS `online_polyclinic`;
 USE `online_polyclinic`;
 
+SET FOREIGN_KEY_CHECKS = 0;
+
 DROP TABLE IF EXISTS `admininstrator`;
 
 --
@@ -15,6 +17,18 @@ CREATE TABLE `admininstrator` (
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
+DROP TABLE IF EXISTS `specialty`;
+
+--
+-- Table structure for table `specialty`
+--
+
+CREATE TABLE `specialty` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+    `name` varchar(100) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
 DROP TABLE IF EXISTS `doctor`;
 
 --
@@ -25,9 +39,13 @@ CREATE TABLE `doctor` (
 	`id` int(11) NOT NULL AUTO_INCREMENT,
     `first_name` varchar(45) DEFAULT NULL,
     `last_name` varchar(45) DEFAULT NULL,
-    `specialty` varchar(45) DEFAULT NULL,
-    PRIMARY KEY (`id`)
+    `specialty_id` int(11) DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    KEY `FK_SPECIALTY_idx` (`specialty_id`),
+    CONSTRAINT `FK_SPECIALTY` FOREIGN KEY (`specialty_id`) REFERENCES `specialty` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+SET FOREIGN_KEY_CHECKS = 1;
 
 DROP TABLE IF EXISTS `patient`;
 
@@ -39,18 +57,6 @@ CREATE TABLE `patient` (
 	`id` int(11) NOT NULL AUTO_INCREMENT,
     `first_name` varchar(45) DEFAULT NULL,
     `last_name` varchar(45) DEFAULT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
-
-DROP TABLE IF EXISTS `specialty`;
-
---
--- Table structure for table `specialty`
---
-
-CREATE TABLE `specialty` (
-	`id` int(11) NOT NULL AUTO_INCREMENT,
-    `name` varchar(45) DEFAULT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
