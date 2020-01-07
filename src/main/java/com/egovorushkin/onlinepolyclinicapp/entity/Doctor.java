91bfe38c2981c3e097d1f1be9ff002ed05625d15
Evgenii <johnnywas87@gmail.com>
2020-01-07 21:35:13 +0300

    Added Doctor entity

--- src/main/java/com/egovorushkin/onlinepolyclinicapp/entity/Doctor.java
+++ src/main/java/com/egovorushkin/onlinepolyclinicapp/entity/Doctor.java
@@ -1,14 +1,18 @@
 package com.egovorushkin.onlinepolyclinicapp.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
-import javax.persistence.SecondaryTable;
 import javax.persistence.Table;
 
 @Entity
@@ -19,20 +23,23 @@ public class Doctor {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private int id;
-	
+
 	@Column(name = "first_name")
 	private String firstName;
-	
+
 	@Column(name = "last_name")
 	private String lastName;
-	
+
 	@OneToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "speciality_id", referencedColumnName = "id")
 	private Speciality speciality;
-	
-	
+
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "doctor", cascade = { CascadeType.PERSIST, CascadeType.MERGE,
+			CascadeType.DETACH, CascadeType.REFRESH })
+	private List<Visit> visits;
+
 	public Doctor() {
-		
+
 	}
 
 	public Doctor(int id, String firstName, String lastName) {
@@ -73,11 +80,28 @@ public class Doctor {
 		this.speciality = speciality;
 	}
 
+	public List<Visit> getVisits() {
+		return visits;
+	}
+
+	public void setVisits(List<Visit> visits) {
+		this.visits = visits;
+	}
+
 	@Override
 	public String toString() {
 		return "Doctor [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", speciality=" + speciality
 				+ "]";
 	}
 
-	
+	public void add(Visit tempVisit) {
+
+		if (visits == null) {
+			visits = new ArrayList<>();
+		}
+
+		visits.add(tempVisit);
+
+		tempVisit.setDoctor(this);
+	}
 }
