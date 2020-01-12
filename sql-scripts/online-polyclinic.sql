9a30f8766da3de2d8b5f3b7bd9158bffc6b20f85
Evgenii <johnnywas87@gmail.com>
2020-01-12 19:34:29 +0300

    fix some errors

--- sql-scripts/online-polyclinic.sql
+++ sql-scripts/online-polyclinic.sql
@@ -9,7 +9,7 @@ DROP TABLE IF EXISTS `admininstrator`;
 -- Table structure for table `admininstrator`
 --
 
-CREATE TABLE `admininstrator` (
+CREATE TABLE `administrator` (
 	`id` int(11) NOT NULL AUTO_INCREMENT,
     `first_name` varchar(45) DEFAULT NULL,
     `last_name` varchar(45) DEFAULT NULL,
@@ -55,20 +55,38 @@ DROP TABLE IF EXISTS `patient`;
 
 CREATE TABLE `patient` (
 	`id` int(11) NOT NULL AUTO_INCREMENT,
-    `first_name` varchar(45) DEFAULT NULL,
-    `last_name` varchar(45) DEFAULT NULL,
+    `first_name` VARCHAR(45) DEFAULT NULL,
+    `last_name` VARCHAR(45) DEFAULT NULL,
+    `address` VARCHAR(100) DEFAULT NULL,
+    `day_of_birth` DATE DEFAULT NULL,
+    `phone_number` VARCHAR(10) DEFAULT NULL,
+    `note` VARCHAR(254) DEFAULT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS `service`;
+DROP TABLE IF EXISTS `medical_service`;
 
 --
--- Table structure for table `service`
+-- Table structure for table `medical_service`
 --
 
-CREATE TABLE `service` (
+CREATE TABLE `medical_service` (
 	`id` int(11) NOT NULL AUTO_INCREMENT,
-    `name` varchar(45) DEFAULT NULL,
+    `name` varchar(100) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `diagnosis`;
+
+--
+-- Table structure for table `diagnosis`
+--
+
+CREATE TABLE `diagnosis` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+    `name` varchar(100) DEFAULT NULL,
+    `description` text DEFAULT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
@@ -80,6 +98,24 @@ DROP TABLE IF EXISTS `visit`;
 
 CREATE TABLE `visit` (
 	`id` int(11) NOT NULL AUTO_INCREMENT,
-    `name` varchar(45) DEFAULT NULL,
-    PRIMARY KEY (`id`)
+    `patient_id` int(11) DEFAULT NULL,
+    `doctor_id` int(11) DEFAULT NULL,
+    `date` datetime DEFAULT NULL,
+    `medical_service_id` int(11) DEFAULT NULL,
+    `diagnosis_id` int(11) DEFAULT NULL,
+    
+    PRIMARY KEY (`id`),
+    
+    CONSTRAINT `FK_PATIENT` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`id`)
+    ON DELETE NO ACTION ON UPDATE NO ACTION,
+    
+    CONSTRAINT `FK_DOCTOR` FOREIGN KEY (`doctor_id`) REFERENCES `doctor` (`id`)
+    ON DELETE NO ACTION ON UPDATE NO ACTION,
+    
+    CONSTRAINT `FK_MEDICAL_SERVICE` FOREIGN KEY (`medical_service_id`) REFERENCES `medical_service` (`id`)
+    ON DELETE NO ACTION ON UPDATE NO ACTION,
+    
+    CONSTRAINT `FK_DIAGNOSIS` FOREIGN KEY (`diagnosis_id`) REFERENCES `diagnosis` (`id`)
+    ON DELETE NO ACTION ON UPDATE NO ACTION
+    
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
